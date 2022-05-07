6de06c2fe243b1290cd2fae01c3fc373a04787f8
skynet-lions <skynet_lions@outlook.com>
2022-05-07 17:05:00 +0800

    Modify

--- Projects/T100/V0.01/Source/T100/include/lib/log/T100Log.h
+++ Projects/T100/V0.01/Source/T100/include/lib/log/T100Log.h
@@ -2,8 +2,8 @@
 #define T100LOG_H
 
 #include <atomic>
-#include "T100Common.h"
-class T100LogAppender;
+#include "T100LogCommon.h"
+#include "T100Hint.h"
 
 
 class T100Log
@@ -12,19 +12,35 @@ class T100Log
         T100Log();
         virtual ~T100Log();
 
-        static T100BOOL                 start();
-        static T100BOOL                 stop();
+        static T100BOOL                     start();
+        static T100BOOL                     stop();
 
-        static T100BOOL                 add(T100LogAppender*);
-        static T100BOOL                 remove(T100LogAppender*);
+        static T100BOOL                     add(T100LogAppender*);
+        static T100BOOL                     remove(T100LogAppender*);
 
-        static T100VOID                 out(T100STDSTRING&);
-        static T100VOID                 out(T100WSTRING&);
+        static T100VOID                     out(T100STDSTRING&);
+        static T100VOID                     out(T100WSTRING&);
 
+        static T100VOID                     fatal(T100STDSTRING);
+        static T100VOID                     error(T100STDSTRING);
+        static T100VOID                     warn(T100STDSTRING);
+        static T100VOID                     debug(T100STDSTRING);
+        static T100VOID                     trace(T100STDSTRING);
+        static T100VOID                     info(T100STDSTRING);
+
+        static T100BOOL                     running();
 
     protected:
+        static T100LOG_APPENDER_VECTOR      m_appenders;
+        static T100LOG_QUEUE                m_queue;
+        static T100LOG_CALLBACK_VECTOR      m_callbacks;
 
     private:
+        static std::atomic_bool             m_running;
+        static T100LOG_TYPE                 m_type;
+
+        static T100VOID                     out(T100LOG_TYPE, T100STDSTRING);
+
 };
 
 #endif // T100LOG_H
--- Projects/T100/V0.01/Source/T100/include/lib/log/T100LogAppender.h
+++ Projects/T100/V0.01/Source/T100/include/lib/log/T100LogAppender.h
@@ -1,12 +1,17 @@
 #ifndef T100LOGAPPENDER_H
 #define T100LOGAPPENDER_H
 
+#include "T100String.h"
+#include "T100LogCommon.h"
+
 
 class T100LogAppender
 {
     public:
         T100LogAppender();
         virtual ~T100LogAppender();
+
+        virtual T100VOID            out(T100WORD, T100STDSTRING)=0;
 
     protected:
 
