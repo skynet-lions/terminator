d66bd9c09243b73256130a85477fd58d9aa67746
skynet-lions <skynet_lions@outlook.com>
2022-07-16 17:52:08 +0800

    Modify

--- Projects/T100/V0.1/Source/T100Assembly/src/scanner/T100ByteScanner.cpp
+++ Projects/T100/V0.1/Source/T100Assembly/src/scanner/T100ByteScanner.cpp
@@ -9,3 +9,38 @@ T100ByteScanner::~T100ByteScanner()
 {
     //dtor
 }
+
+T100BOOL T100ByteScanner::open(T100WSTRING file)
+{
+    return T100FALSE;
+}
+
+T100BOOL T100ByteScanner::close()
+{
+    return T100FALSE;
+}
+
+T100BOOL T100ByteScanner::next(T100Token& token)
+{
+    return T100FALSE;
+}
+
+T100BOOL T100ByteScanner::run()
+{
+    return T100FALSE;
+}
+
+T100BOOL T100ByteScanner::read()
+{
+    return T100FALSE;
+}
+
+T100ByteToken* T100ByteScanner::getToken()
+{
+    return m_token;
+}
+
+T100TOKEN_TYPE T100ByteScanner::classify(T100BYTE byte)
+{
+
+}
