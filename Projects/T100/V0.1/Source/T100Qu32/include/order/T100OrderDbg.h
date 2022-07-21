ed26c845278707a64f79fa5dcd8ac62dcc7f7516
skynet-lions <skynet_lions@outlook.com>
2022-07-21 11:28:51 +0800

    Modify

---
+++ Projects/T100/V0.1/Source/T100Qu32/include/order/T100OrderDbg.h
@@ -0,0 +1,16 @@
+#ifndef T100ORDERDBG_H
+#define T100ORDERDBG_H
+
+
+class T100OrderDbg
+{
+    public:
+        T100OrderDbg();
+        virtual ~T100OrderDbg();
+
+    protected:
+
+    private:
+};
+
+#endif // T100ORDERDBG_H
---
+++ Projects/T100/V0.1/Source/T100Qu32/src/order/T100OrderDbg.cpp
@@ -0,0 +1,11 @@
+#include "T100OrderDbg.h"
+
+T100OrderDbg::T100OrderDbg()
+{
+    //ctor
+}
+
+T100OrderDbg::~T100OrderDbg()
+{
+    //dtor
+}
